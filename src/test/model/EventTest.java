548f5f4c9a7ef797f46019c7a43b09a53999904c
byeorikim <bkim10@student.ubc.ca>
2023-08-08 23:05:27 -0700

    tests for event and event log

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -7,10 +7,12 @@ import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class EventTest {
     private Event e;
     private Date d;
+    private Event e2;
 
     //NOTE: these tests might fail if time at which line (2) below is executed
     //is different from time that line (1) is executed.  Lines (1) and (2) must
@@ -20,8 +22,24 @@ public class EventTest {
     public void runBefore() {
         e = new Event("Sensor open at door");   // (1)
         d = Calendar.getInstance().getTime();   // (2)
+        e2 = new Event("Sensor open at door");
     }
 
+    @Test
+    public void testHashCode() {
+        assertEquals(e2.hashCode(), e.hashCode());
+    }
+
+    @Test
+    public void testEqualsNull() {
+        assertFalse(e.equals(null));
+    }
+
+    @Test
+    public void testEqualsClass() {
+        SneakerList s = new SneakerList();
+        assertFalse(e.equals(s));
+    }
     @Test
     public void testEvent() {
         assertEquals("Sensor open at door", e.getDescription());
